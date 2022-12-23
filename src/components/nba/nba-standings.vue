787be9134838830268cf8ff0c73dbf01f8c68aef
Alan Killian <akillian@outlook.com>
2022-12-23 18:30:54 -0500

    Start standings component.

--- src/components/nba/nba-standings.vue
+++ src/components/nba/nba-standings.vue
@@ -1,5 +1,8 @@
 <template>
   <h1>NBA standings</h1>
+  <ul v-for="(member, index) in nbaTeamStandings" :key="member.position">
+    <li>{{ index }} - {{ member.team.name }} <img :src="member.team.logo" alt="Team Logo"/></li>
+  </ul>
 </template>
 
 <script setup>
@@ -8,8 +11,7 @@ import { ref } from "vue";
 const API_KEY = import.meta.env.VITE_API_SPORTS_KEY;
 const HOST_NAME = import.meta.env.VITE_API_HOST;
 
-const teamInfo = ref([]);
-const teamStandings = ref([]);
+const nbaTeamStandings = ref([]);
 
 let myHeaders = new Headers();
 myHeaders.append("x-apisports-key", API_KEY);
@@ -22,15 +24,23 @@ const requestOptions = {
   redirect: "follow",
 };
 
-const urlTeamStandings =
+const urlNBATeamStandings =
   "https://v1.basketball.api-sports.io/standings?league=12&season=2022-2023";
 
 // fetch the NBA team standings
-fetch(urlTeamStandings, requestOptions)
+fetch(urlNBATeamStandings, requestOptions)
   .then((response) => response.json())
   .then((data) => {
-    teamInfo.value = data.response[0];
-    console.log(teamInfo.value[22].group.name);
+    nbaTeamStandings.value = data.response[0];
+    console.log(nbaTeamStandings.value[22].group.name);
   })
   .catch((error) => console.log("Error fetching data ==>", error));
 </script>
+
+
+<style scoped>
+  img[alt="Team Logo"] {
+    width: 1rem;
+  }
+
+</style>
