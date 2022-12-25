e08c5e5bbc17e53d7ac68422e5a06f939d5ea209
Alan Killian <akillian@outlook.com>
2022-12-25 18:36:18 -0500

    commit

--- src/components/nba/nba-standings.vue
+++ src/components/nba/nba-standings.vue
@@ -1,7 +1,10 @@
 <template>
   <h1>NBA standings</h1>
   <ul v-for="(member, index) in nbaTeamStandings" :key="member.position">
-    <li>{{ index }} - {{ member.team.name }} <img :src="member.team.logo" alt="Team Logo"/></li>
+    <li>
+      {{ index }} - {{ member.team.name }}
+      <img :src="member.team.logo" alt="Team Logo" />
+    </li>
   </ul>
 </template>
 
@@ -11,12 +14,23 @@ import { ref } from "vue";
 const API_KEY = import.meta.env.VITE_API_SPORTS_KEY;
 const HOST_NAME = import.meta.env.VITE_API_HOST;
 
-const nbaTeamStandings = ref([]);
+const nbaDivisions = ref([
+  ["Atlantic", "Southeast", "Central"],
+  ["Northwest", "Pacific", "Southwest"],
+]);
 
+console.log(nbaDivisions.value[0].length);
+let urls = [];
+// Populate the urls array with url's of each division for API fetch's
+for (let i = 0; i < nbaDivisions.value.length; i++) {
+  for (let n = 0; n < nbaDivisions.value[i].length; n++) {
+    urls = urls.push(nbaDivisions.value[i][n]);
+  }
+}
+console.log(urls);
 let myHeaders = new Headers();
 myHeaders.append("x-apisports-key", API_KEY);
 myHeaders.append("x-rapidapi-host", HOST_NAME);
-console.log(`API key is ${myHeaders.get("x-apisports-key")}`);
 
 const requestOptions = {
   method: "GET",
@@ -28,19 +42,17 @@ const urlNBATeamStandings =
   "https://v1.basketball.api-sports.io/standings?league=12&season=2022-2023";
 
 // fetch the NBA team standings
-fetch(urlNBATeamStandings, requestOptions)
+/* fetch(urlNBATeamStandings, requestOptions)
   .then((response) => response.json())
   .then((data) => {
     nbaTeamStandings.value = data.response[0];
     console.log(nbaTeamStandings.value[22].group.name);
   })
-  .catch((error) => console.log("Error fetching data ==>", error));
+  .catch((error) => console.log("Error fetching data ==>", error)); */
 </script>
 
-
 <style scoped>
-  img[alt="Team Logo"] {
-    width: 1rem;
-  }
-
+img[alt="Team Logo"] {
+  width: 1rem;
+}
 </style>
