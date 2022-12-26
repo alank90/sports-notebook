6a1396e28edc4af28ee54818fdbafbe05c715117
Alan Killian <akillian@outlook.com>
2022-12-26 17:33:25 -0500

    commit

--- src/components/nba/nba-standings.vue
+++ src/components/nba/nba-standings.vue
@@ -1,16 +1,17 @@
 <template>
   <h1>NBA standings</h1>
-  <ul v-for="(member, index) in nbaTeamStandings" :key="member.position">
+  <!-- <ul v-for="(member, index) in nbaTeamStandings" :key="member.position">
     <li>
       {{ index }} - {{ member.team.name }}
       <img :src="member.team.logo" alt="Team Logo" />
     </li>
-  </ul>
+  </ul> -->
 </template>
 
 <script setup>
 import { ref } from "vue";
 
+// ======= Variable Declarations ============ //
 const API_KEY = import.meta.env.VITE_API_SPORTS_KEY;
 const HOST_NAME = import.meta.env.VITE_API_HOST;
 
@@ -19,15 +20,8 @@ const nbaDivisions = ref([
   ["Northwest", "Pacific", "Southwest"],
 ]);
 
-console.log(nbaDivisions.value[0].length);
-let urls = [];
-// Populate the urls array with url's of each division for API fetch's
-for (let i = 0; i < nbaDivisions.value.length; i++) {
-  for (let n = 0; n < nbaDivisions.value[i].length; n++) {
-    urls = urls.push(nbaDivisions.value[i][n]);
-  }
-}
-console.log(urls);
+const nbaTeamStandingsByDivision = ref([]);
+
 let myHeaders = new Headers();
 myHeaders.append("x-apisports-key", API_KEY);
 myHeaders.append("x-rapidapi-host", HOST_NAME);
@@ -38,10 +32,23 @@ const requestOptions = {
   redirect: "follow",
 };
 
-const urlNBATeamStandings =
-  "https://v1.basketball.api-sports.io/standings?league=12&season=2022-2023";
-
+let urls = [];
+// Populate the urls array with url's of each division for API fetch's
+for (let i = 0; i < nbaDivisions.value.length; i++) {
+  for (let n = 0; n < nbaDivisions.value[i].length; n++) {
+    urls.push(
+      fetch(
+        `https://v1.basketball.api-sports.io/standings/?league=12&group=${nbaDivisions.value[i][n]}&season=2022-2023`,
+        requestOptions
+      )
+    );
+  }
+}
 // fetch the NBA team standings
+Promise.all(urls)
+  .then((values) => console.log(values))
+  .catch((error) => console.log("Error fetching data ==>", error));
+
 /* fetch(urlNBATeamStandings, requestOptions)
   .then((response) => response.json())
   .then((data) => {
