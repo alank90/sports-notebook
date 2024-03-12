8b5a1c030c5cf98e0b32f4b6b8c538b89c1a0338
Alan K <akillian@outlook.com>
2024-03-12 14:25:54 -0400

    commit misc..

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    env: {
+        node: true,
+        es2021: true,
+    },
+    extends: ["eslint:recommended", "plugin:vue/essential", "prettier"],
+    parserOptions: {
+        ecmaVersion: 13,
+        sourceType: "module",
+    },
+    plugins: ["vue"],
+    rules: {
+        "vue/no-multiple-template-root": "off",
+        "vue/multi-word-component-names": [
+            "error",
+            {
+                ignores: ["Home", "About"],
+            },
+        ],
+    },
+};
